e13d1d7b178a38048c166d4dce4c54a6c6de7f93
martenzoe <biz.zoellner@gmail.com>
2025-01-29 17:53:52 +0100

    Added the delete funtionality

--- app.py
+++ app.py
@@ -39,5 +39,20 @@ def add():
 
     return render_template('add.html')
 
+
+@app.route('/delete/<int:post_id>')
+def delete(post_id):
+    global blog_posts
+    # Finden und Entfernen des Blog-Posts mit der gegebenen ID
+    blog_posts = [post for post in blog_posts if post['id'] != post_id]
+
+    # Aktualisieren der JSON-Datei
+    with open('blog_posts.json', 'w') as file:
+        json.dump(blog_posts, file, indent=4)
+
+    # Zurück zur Startseite
+    return redirect(url_for('index'))
+
+
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5001, debug=True)
