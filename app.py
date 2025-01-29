0c76aa98cafc8840c6c6dce183b8fa4a5382498b
martenzoe <biz.zoellner@gmail.com>
2025-01-29 18:31:07 +0100

    Changing Ports back to 5000

--- app.py
+++ app.py
@@ -145,4 +145,4 @@ if __name__ == '__main__':
     """
     Run the Flask application on host 0.0.0.0 and port 5001 in debug mode.
     """
-    app.run(host="0.0.0.0", port=5001, debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
